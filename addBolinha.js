d2e6507eb349417e0b0705334f6589995fb83abc
torezani89 <torezani89@outlook.com>
2021-08-09 11:30:00 -0300

    Create addBolinha.js

---
+++ addBolinha.js
@@ -0,0 +1,17 @@
+// Ao carregar uma imagem, quero criar uma bolinha:
+// <div class="bolinha" id=""></div>
+// A id da bolinha deve receber o valor da propriedade 'name' da imagem.
+
+// Pegar o array das imagens, fazer um forEach nele e adicionar uma bolinha para cada item do array
+// A bolinha deve ser adicionada dentro da div 'container-bolinhas'
+
+const containerBolinhas = document.getElementById('container-bolinhas');
+
+function addBolinha(image) {
+    let criaBolinha = document.createElement('div');
+    criaBolinha.classList.add('bolinha');
+    criaBolinha.id = image.name;
+    containerBolinhas.appendChild(criaBolinha);
+}
+
+export {addBolinha};
