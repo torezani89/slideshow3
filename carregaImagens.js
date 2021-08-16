0bf22b0e3651957f9352ac6e60abf57fc4e011c8
torezani89 <torezani89@outlook.com>
2021-08-16 10:52:06 -0300

    Update carregaImagens.js

--- carregaImagens.js
+++ carregaImagens.js
@@ -1,4 +1,7 @@
-import {addBolinha} from "./addBolinha.js";
+import {addBolinha, resetBolinhas} from "./addBolinha.js";
+import { marcaBolinha } from "./marcaBolinha.js";
+// import { newImage } from "./addImagem.js";
+
 
 const images = [
     { 'name': 'chrono', 'url':'./img/chrono.jpg' },
@@ -9,11 +12,19 @@ const images = [
     { 'name': 'ippo', 'url':'./img/ippo.png' },
 ]
 
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ADD IMAGE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< LOADIMAGES >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 // const containerItems = document.querySelector('#container-items');
 /* containerItems é passado para loadImages como parametro (container). Ele é passado no momento de chamar loadImages.
- loadImages é chamado em slideShow3.js. Portanto, containerItems deve ser declarado lá. */
+loadImages é chamado em slideShow3.js. Portanto, containerItems deve ser declarado lá. */
 
 const loadImages = function (images, container) {
+
+    resetBolinhas();
+
     images.forEach(image => {
         // Observe o operador (+=) para que ele acumule os conteúdos (image) e não substitua (caso colocássemos só ()=) )
         container.innerHTML += `
@@ -21,8 +32,18 @@ const loadImages = function (images, container) {
                 <img class='imagem' src='${image.url}'
             </div>
         `
+
         addBolinha(image);
     });
+
+    marcaBolinha();
 }
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CRIA IMAGEM >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXPORT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 export {images, loadImages}
